3795a23738073a19b3a1376b94e0f15fc27668d2
serhatprogramming <serhatprogramming@gmail.com>
2023-06-13 18:59:22 -0400

    .

--- part3/phonebook/src/services/persons.js
+++ part3/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api/persons";
+const API_URL = "/api/persons";
 
 const getPersons = () => {
   const response = axios.get(API_URL);
