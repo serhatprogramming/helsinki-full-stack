56dee4b88fcbe23297640cec26b2199aabc9ac1f
serhatprogramming <serhatprogramming@gmail.com>
2023-06-20 21:25:28 -0400

    app organized.

--- part4/blog-list-application/app.js
+++ part4/blog-list-application/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
-const config = require("./utils/config");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
 
 mongoose
   .connect(config.MONGODB_URL)
@@ -12,5 +15,15 @@ mongoose
   .catch((err) => {
     logger.error("Error connecting to database");
   });
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
--- part4/blog-list-application/index.js
+++ part4/blog-list-application/index.js
@@ -1,13 +1,6 @@
-const express = require("express");
 const app = require("./app");
-const cors = require("cors");
-const blogsRouter = require("./controllers/blogs");
-const config = require("./utils/config");
 const logger = require("./utils/logger");
-
-app.use(cors());
-app.use(express.json());
-app.use("/api/blogs", blogsRouter);
+const config = require("./utils/config");
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
