bb28557186fd5d24cfc1157c2115be9b81450da7
serhatprogramming <serhatprogramming@gmail.com>
2023-06-21 12:27:06 -0400

    testHelper added.

--- part4/blog-list-application/tests/blog_api.test.js
+++ part4/blog-list-application/tests/blog_api.test.js
@@ -1,14 +1,30 @@
 const mongoose = require("mongoose");
 const app = require("../app");
 const supertest = require("supertest");
+const Blog = require("../models/blog");
+const testHelper = require("../utils/blog_api_test_helper");
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  const promiseArray = testHelper.initialBlogs.map((blog) => {
+    const blogMongooseObject = new Blog(blog);
+    return blogMongooseObject.save();
+  });
+  await Promise.all(promiseArray);
+});
 
 test("blogs returned as JSON", async () => {
   await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+});
+
+test("There are 6 blog instances in the database", async () => {
+  const blogs = await testHelper.blogsInDb();
+  expect(blogs.length).toBe(6);
 });
 
 afterAll(async () => {
