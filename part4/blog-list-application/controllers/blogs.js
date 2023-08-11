40e8cf9088bc33cb126bed6b4d7d6e02891c3b85
serhatprogramming <serhatprogramming@gmail.com>
2023-08-11 19:35:09 -0400

    token extractor middleware

--- part4/blog-list-application/controllers/blogs.js
+++ part4/blog-list-application/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", {
@@ -19,8 +20,14 @@ blogsRouter.get("/:id", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const { title, author, url, likes, user } = request.body;
-  const retrievedUser = await User.findById(user);
+  const { title, author, url, likes } = request.body;
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET_KEY);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const retrievedUser = await User.findById(decodedToken.id);
+
   if (!title || !url) {
     return response.status(400).json({
       status: 400,
--- part4/blog-list-application/utils/middleware.js
+++ part4/blog-list-application/utils/middleware.js
@@ -16,12 +16,24 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    request.token = authorization.replace("Bearer ", "");
+  } else {
+    request.token = null;
+  }
+  next();
+};
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
     return response.status(400).json({ error: error.message });
   }
 
@@ -32,4 +44,5 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 };
