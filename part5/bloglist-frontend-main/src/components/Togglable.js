f949b9f64e5dbc4b27cbb020d6d91f0e90c0093b
serhatprogramming <serhatprogramming@gmail.com>
2023-08-21 20:12:13 -0400

    eslint prop types

--- part5/bloglist-frontend-main/src/components/Togglable.js
+++ part5/bloglist-frontend-main/src/components/Togglable.js
@@ -1,5 +1,7 @@
 import { useState, forwardRef, useImperativeHandle } from "react";
 
+import PropTypes from "prop-types";
+
 const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false);
 
@@ -28,5 +30,9 @@ const Togglable = forwardRef((props, refs) => {
     </div>
   );
 });
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
 
 export default Togglable;
