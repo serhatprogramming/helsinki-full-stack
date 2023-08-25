b43555cf58409d11d8c7155b33ad04323da79447
serhatprogramming <serhatprogramming@gmail.com>
2023-08-25 19:13:16 -0400

    anecdote with query

--- part6/query-anecdotes-main/src/App.jsx
+++ part6/query-anecdotes-main/src/App.jsx
@@ -1,40 +1,53 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAnecdotes, updateAnecdote } from "./requests";
 
 const App = () => {
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: () => getAnecdotes(),
+  });
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
+  const queryClient = useQueryClient();
+
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+  });
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
+  }
+  if (result.status === "error") {
+    return <div>anecdote service not available due to problems in server</div>;
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const anecdotes = result.data;
+
+  const handleVote = (anecdote) => {
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
+  };
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ part6/query-anecdotes-main/src/requests.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/anecdotes";
+
+export const getAnecdotes = () => axios.get(baseUrl).then((res) => res.data);
+
+export const createAnecdote = (newAnecdote) => {
+  if (newAnecdote.content.length < 5) {
+    return;
+  }
+  axios.post(baseUrl, newAnecdote).then((res) => res.data);
+};
+
+export const updateAnecdote = (updatedAnecdote) =>
+  axios
+    .put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote)
+    .then((res) => res.data);
