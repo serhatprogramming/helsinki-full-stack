1c168c65654f07391c3e9fcab535faf88ed541cd
serhatprogramming <serhatprogramming@gmail.com>
2023-09-02 16:54:39 -0400

    create new styled.

--- part7/bloglist-frontend-redux/src/components/BlogForm.js
+++ part7/bloglist-frontend-redux/src/components/BlogForm.js
@@ -15,40 +15,51 @@ const BlogForm = ({ handleCreateBlog }) => {
 
   return (
     <>
-      <form onSubmit={createBlog}>
-        <h2>create new</h2>
-        <div>
-          title:
-          <input
-            type="text"
-            value={title}
-            name="Title"
-            onChange={({ target }) => setTitle(target.value)}
-            id="title-input"
-          />
-        </div>
-        <div>
-          author:
-          <input
-            type="text"
-            value={author}
-            name="Author"
-            onChange={({ target }) => setAuthor(target.value)}
-            id="author-input"
-          />
-        </div>
-        <div>
-          url:
-          <input
-            type="text"
-            value={url}
-            name="Url"
-            onChange={({ target }) => setUrl(target.value)}
-            id="url-input"
-          />
+      <form onSubmit={createBlog} className="create-blog-form">
+        <h2>Create New Blog</h2>
+        <div className="form-fields">
+          <div className="form-field">
+            <label htmlFor="title-input" className="form-label">
+              Title:
+            </label>
+            <input
+              type="text"
+              value={title}
+              name="Title"
+              onChange={({ target }) => setTitle(target.value)}
+              id="title-input"
+              className="form-input"
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="author-input" className="form-label">
+              Author:
+            </label>
+            <input
+              type="text"
+              value={author}
+              name="Author"
+              onChange={({ target }) => setAuthor(target.value)}
+              id="author-input"
+              className="form-input"
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="url-input" className="form-label">
+              Url:
+            </label>
+            <input
+              type="text"
+              value={url}
+              name="Url"
+              onChange={({ target }) => setUrl(target.value)}
+              id="url-input"
+              className="form-input"
+            />
+          </div>
         </div>
         <button type="submit" id="create-button">
-          create
+          Create
         </button>
       </form>
     </>
